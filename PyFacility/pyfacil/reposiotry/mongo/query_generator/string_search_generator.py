0d41710f7ebd6bc4fe0344dde02f75137d4d99b1
r-azh <r.azh.777@gmail.com>
2016-04-13 09:16:34 +0430

    add case insensitivity to pyfacil string search generator.

--- PyFacility/pyfacil/reposiotry/mongo/query_generator/string_search_generator.py
+++ PyFacility/pyfacil/reposiotry/mongo/query_generator/string_search_generator.py
@@ -13,9 +13,11 @@ class StringSearchGenerator:
 
     def generate(self, value):
         if not self.split_string:
-            return {self.field_name: {'$regex': '.*{}.*'.format(value)}}
+            return {self.field_name: {'$regex': '.*{}.*'.format(value),
+                                       '$options': '-i'}}
         else:
             text_criteria = {"${}".format(self.string_splitter_operator): []}
             for txt in value.split(" "):
-                text_criteria["${}".format(self.string_splitter_operator)].append({self.field_name: {'$regex': '.*{}.*'.format(txt)}})
+                text_criteria["${}".format(self.string_splitter_operator)].append({self.field_name: {'$regex': '.*{}.*'.format(txt),
+                                                                                                      '$options': '-i'}})
             return text_criteria
