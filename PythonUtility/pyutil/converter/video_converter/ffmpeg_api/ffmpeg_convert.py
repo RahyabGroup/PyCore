23dfd5765a0a58155340eb8f79c247caa383574b
r-azh <r.azh.777@gmail.com>
2017-02-04 18:00:41 +0330

    slight changes in video converter by ffmpeg.

--- PythonUtility/pyutil/converter/video_converter/ffmpeg_api/ffmpeg_convert.py
+++ PythonUtility/pyutil/converter/video_converter/ffmpeg_api/ffmpeg_convert.py
@@ -6,7 +6,7 @@ FFMPEG_PATH = 'ffmpeg'
 
 def encode(file, video_configs, output_file):
     command = [FFMPEG_PATH, '-i', file]
-    command.append(video_configs)
+    command += video_configs
     command += ['-threads', '8', output_file]    # add threads and output
     subprocess.call(command)    # encode the video!
 
