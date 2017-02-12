d66d73800053c65d7c01ae888e3727f07f833259
r-azh <r.azh.777@gmail.com>
2017-02-12 15:05:21 +0330

    changes in thread for convert to mp4.

--- PythonUtility/pyutil/converter/video_converter/ffmpeg_api/video_converter.py
+++ PythonUtility/pyutil/converter/video_converter/ffmpeg_api/video_converter.py
@@ -1,16 +1,19 @@
+import subprocess
 from pyutil.converter.video_converter.ffmpeg_api.ffmpeg_convert_api import encode
 
 __author__ = 'root'
 
 
-def convert_to_mp4(file_path, file_name, output_path=None, output_file_name=None):
-    output_path = file_path if not output_path else output_path
+def convert_to_mp4(source_file_path, source_file_name, output_path=None, output_file_name=None, remove_source_file=False):
+    output_path = source_file_path if not output_path else output_path
     mp4_configs = ['-codec:v', 'libx264', '-profile:v', 'high', '-codec:a', 'libfdk_aac']
     if not output_file_name:
-        output_file_name = ''.join(file_name.split('.')[:-1])
+        output_file_name = ''.join(source_file_name.split('.')[:-1])
     converted_file_name = '{}.{}'.format(output_file_name, 'mp4')
-    encode('{}/{}'.format(file_path, file_name), mp4_configs,
+    encode('{}/{}'.format(source_file_path, source_file_name), mp4_configs,
            '{}/{}'.format(output_path, converted_file_name))
+    if remove_source_file:
+        _remove_file(source_file_path, source_file_name)
     return converted_file_name
 
 
@@ -19,7 +22,7 @@ def convert_to_flv(file_path, file_name, output_path=None):
     flv_configs = ['-c:v', 'libx264', '-ar', '22050', '-crf', '28']
     converted_file_name = '{}.{}'.format(''.join(file_name.split('.')[:-1]), 'flv')
     encode('{}/{}'.format(file_path, file_name), flv_configs,
-                                 '{}/{}'.format(output_path, converted_file_name))
+           '{}/{}'.format(output_path, converted_file_name))
     return converted_file_name
 
 
@@ -28,7 +31,7 @@ def convert_to_webm(file_path, file_name, output_path=None):
     webm_configs = ['-b', '1500k', '-vcodec', 'libvpx', '-acodec', 'libvorbis', '-ab', '160000', '-f', 'webm', '-g', '30']
     converted_file_name = '{}.{}'.format(''.join(file_name.split('.')[:-1]), 'webm')
     encode('{}/{}'.format(file_path, file_name), webm_configs,
-                                 '{}/{}'.format(output_path, converted_file_name))
+           '{}/{}'.format(output_path, converted_file_name))
     return converted_file_name
 
 
@@ -37,5 +40,10 @@ def convert_to_ogg(file_path, file_name, output_path=None):
     ogg_configs = ['-acodec', 'libvorbis']
     converted_file_name = '{}.{}'.format(''.join(file_name.split('.')[:-1]), 'ogg')
     encode('{}/{}'.format(file_path, file_name), ogg_configs,
-                                 '{}/{}'.format(output_path, converted_file_name))
+           '{}/{}'.format(output_path, converted_file_name))
     return converted_file_name
+
+
+def _remove_file(file_path, file_name):
+    command = ['rm', '{}/{}'.format(file_path, file_name)]
+    subprocess.call(command)
