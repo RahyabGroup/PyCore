bcf9df4b6615d855f383cfb7b44fb929e24cdfa4
r-azh <r_azh_777@yahoo.com>
2017-05-09 12:53:40 +0430

    added field name to validation.

--- PythonValidation/pyvalidate/validation_container.py
+++ PythonValidation/pyvalidate/validation_container.py
@@ -7,7 +7,7 @@ class ValidationContainer:
     def __init__(self, validation_result):
         self._validation_result = validation_result
 
-    def _execute(self, item_to_validate, validator, none_exception):
+    def _execute(self, item_to_validate, validator, none_exception, field_name=None):
         if item_to_validate is None:
             if none_exception:
                 result = ErrorCodes.ITEM_IS_NONE
@@ -20,8 +20,13 @@ class ValidationContainer:
         result = validator.validate(item_to_validate)
         if result is not None:
             if isinstance(result, list):
+                if field_name:
+                    for r in result:
+                        r['field_name'] = field_name
                 self._validation_result.extend(result)
             else:
+                if field_name:
+                    result['field_name'] = field_name
                 self._validation_result.append(result)
         return result
 
