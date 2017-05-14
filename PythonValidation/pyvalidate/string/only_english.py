df473b2f4358c23f4208674d94fce3fce1586ca0
r-azh <r_azh_777@yahoo.com>
2017-05-14 10:10:49 +0430

    added punctuatio characters to only persian-english.

--- PythonValidation/pyvalidate/string/only_english.py
+++ PythonValidation/pyvalidate/string/only_english.py
@@ -1,4 +1,5 @@
 import re
+import string
 from pyvalidate.create_result_message import create_result_message
 
 from pyvalidate.resources import ErrorCodes
@@ -8,8 +9,9 @@ __author__ = 'root'
 
 class OnlyEnglish:
     def validate(self, item_to_validate, field_name=None):
-        only_string_pattern = '^[a-zA-Z \n]+$'
-        if not re.match(only_string_pattern, item_to_validate):
+        punc = '\\' + '\\'.join(string.punctuation)
+        only_english_string_pattern = '^[{} 0-9a-zA-Z \n]+$'.format(punc)
+        if not re.match(only_english_string_pattern, item_to_validate):
             result = create_result_message(ErrorCodes.ITEM_IS_NOT_ONLY_ENGLISH, field_name, item_to_validate)
             return result
         return None
--- PythonValidation/pyvalidate/string/only_persian.py
+++ PythonValidation/pyvalidate/string/only_persian.py
@@ -1,4 +1,5 @@
 import re
+import string
 from pyvalidate.create_result_message import create_result_message
 from pyvalidate.resources import ErrorCodes
 
@@ -9,8 +10,10 @@ class OnlyPersian:
     def validate(self, item_to_validate, field_name=None):
         farsi_alef_char = chr(1570)
         farsi_ye_char = chr(1740)
-        only_string_pattern = '^[{}-{} \n]+$'.format(farsi_alef_char, farsi_ye_char)
-        if not re.match(only_string_pattern, item_to_validate):
+        punc = '\\' + '\\'.join(string.punctuation)
+        only_persian_string_pattern = '^[{} 0-9 {}-{} \n]+$'.format(punc, farsi_alef_char,
+                                                                    farsi_ye_char)
+        if not re.match(only_persian_string_pattern, item_to_validate):
             result = create_result_message(ErrorCodes.ITEM_IS_NOT_ONLY_PERSIAN, field_name, item_to_validate)
             return result
         return None
