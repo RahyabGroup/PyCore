6472f556e8427aa1ef1b0ac3312091256018b8a9
r-azh <r_azh_777@yahoo.com>
2017-05-14 10:11:20 +0430

    added str() to prevent problems

--- PythonValidation/pyvalidate/string/object_id.py
+++ PythonValidation/pyvalidate/string/object_id.py
@@ -8,7 +8,8 @@ __author__ = 'root'
 class ObjectId:
     def validate(self, item_to_validate, field_name=None):
         object_id_pattern = '^[0-9a-f]{24}'
-        if not re.match(object_id_pattern, item_to_validate):
+        #fixme
+        if not re.match(object_id_pattern, str(item_to_validate)):
             result = create_result_message(ErrorCodes.OBJECT_ID_NOT_VALID, field_name, item_to_validate)
             return result
         return None
