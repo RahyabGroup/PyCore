22c873dbb881e81b3d356634ee277e77b4874cd7
r-azh <r_azh_777@yahoo.com>
2017-07-25 14:06:22 +0430

    added connect=false to mongo connection.

--- PythonDataAccessLayer/pydal/mongo/connection.py
+++ PythonDataAccessLayer/pydal/mongo/connection.py
@@ -9,7 +9,7 @@ __author__ = 'Hooman'
 class Connection:
     def __init__(self, connection_string):
         self._connection_string = connection_string
-        self._mongo_connection = MongoClient(connection_string)
+        self._mongo_connection = MongoClient(connection_string, connect=False)
 
     def db(self, name):
         mongodb = self._mongo_connection[name]
